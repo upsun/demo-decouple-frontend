9404dae3359900c63bc63014263a779c33cf92b1
Florent Huck <flovntp@gmail.com>
2025-04-18 10:02:55 +0200

    enhanced scripts

--- clamav/scripts/clam_install.sh
+++ clamav/scripts/clam_install.sh
@@ -5,7 +5,9 @@ echo "Prepare folder for clamav..."
 mkdir -p \
     "${PLATFORM_APP_DIR}/var/log" \
     "${PLATFORM_APP_DIR}/var/lib" \
-    "${PLATFORM_APP_DIR}/var/etc"
+    "${PLATFORM_APP_DIR}/var/etc" \
+    "${PLATFORM_APP_DIR}/mydata/folder2scan" \
+    "${PLATFORM_APP_DIR}/mydata/quarantine" 
 
 echo "Move config on mount..."
 cp "${PLATFORM_APP_DIR}/etc/clamd.conf" "${PLATFORM_APP_DIR}/var/etc/"
